baf359e941148fea76e289d397c671228aa8e2bb
Adeyemi Dada <yemidada@gmail.com>
2024-01-18 20:28:55 +0100

    Update files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/Users/mac/Documents/web/microverse/hello-rails-backend/LICENSE/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
