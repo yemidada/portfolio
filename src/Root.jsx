877329a967adf29d14a3d4962afc882b1e6ef7e7
Adeyemi Dada <yemidada@gmail.com>
2024-01-18 21:14:30 +0100

    fix linter error

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
-import logo from "./assets/logo.png";
-import pic from "./assets/pic.jpg";
-import { TypeAnimation } from "react-type-animation";
+import { useState } from 'react';
+import { TypeAnimation } from 'react-type-animation';
 import {
   BiSolidHome,
   BiSolidFoodMenu,
@@ -10,47 +8,49 @@ import {
   BiLogoGithub,
   BiLogoLinkedinSquare,
   BiLogoTwitter,
-} from "react-icons/bi";
-import HomePage from "./pages/HomePage";
+} from 'react-icons/bi';
+import logo from './assets/logo.png';
+import pic from './assets/pic.jpg';
+import HomePage from './pages/HomePage';
 
-export function Root() {
+function Root() {
   const [active, setActive] = useState(false);
-  const [menu, setMenu] = useState("Home");
+  const [menu, setMenu] = useState('Home');
 
   const links = [
     {
-      name: "Home",
-      link: "#",
+      name: 'Home',
+      link: '#',
       icon: (
-        <BiSolidHome color={active === "Home" ? "#1e3a8a" : ""} size={24} />
+        <BiSolidHome color={active === 'Home' ? '#1e3a8a' : ''} size={24} />
       ),
     },
     {
-      name: "Experience",
-      link: "#resume",
+      name: 'Experience',
+      link: '#resume',
       icon: (
         <BiSolidFoodMenu
-          color={active === "Experience" ? "#1e3a8a" : ""}
+          color={active === 'Experience' ? '#1e3a8a' : ''}
           size={24}
         />
       ),
     },
     {
-      name: "Projects",
-      link: "#works",
+      name: 'Projects',
+      link: '#works',
       icon: (
         <BiSolidBriefcase
-          color={active === "Projects" ? "#1e3a8a" : ""}
+          color={active === 'Projects' ? '#1e3a8a' : ''}
           size={24}
         />
       ),
     },
     {
-      name: "Contact",
-      link: "#contact",
+      name: 'Contact',
+      link: '#contact',
       icon: (
         <BiSolidContact
-          color={active === "Contact" ? "#1e3a8a" : ""}
+          color={active === 'Contact' ? '#1e3a8a' : ''}
           size={24}
         />
       ),
@@ -61,7 +61,7 @@ export function Root() {
       <nav className="sticky top-0 z-30  border-gray-200 mb-10 text-[30px] shadow-lg rounded-2xl bg-white p-4">
         <div className="w-full mx-auto">
           <div className="mx-2 flex flex-wrap items-center justify-between">
-            <img width={80} src={logo} />
+            <img width={80} src={logo} alt="logo" />
             <div className="flex md:hidden md:order-2">
               <button
                 onClick={() => {
@@ -71,51 +71,62 @@ export function Root() {
                 type="button"
                 className="md:hidden text-gray-400 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center"
                 aria-controls="mobile-menu-3"
-                aria-expanded="false">
+                aria-expanded="false"
+              >
                 <span className="sr-only">Open main menu</span>
                 <svg
                   className="w-6 h-6"
                   fill="currentColor"
                   viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg">
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path
                     fillRule="evenodd"
                     d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                    clipRule="evenodd"></path>
+                    clipRule="evenodd"
+                  />
                 </svg>
                 <svg
                   className="hidden w-6 h-6"
                   fill="currentColor"
                   viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg">
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path
                     fillRule="evenodd"
                     d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"></path>
+                    clipRule="evenodd"
+                  />
                 </svg>
               </button>
             </div>
             <div
               className={`${
-                !menu ? "block" : "hidden"
+                !menu ? 'block' : 'hidden'
               } md:flex justify-between items-end w-full md:w-auto md:order-1`}
-              id="mobile-menu-3">
+              id="mobile-menu-3"
+            >
               <ul className="flex-col gap-4 md:flex-row flex md:space-x-12 mt-4 md:mt-0 md:text-sm md:font-medium">
-                {links.map((row, index) => {
-                  return (
-                    <li key={index}>
-                      <a
-                        href={row.link}
-                        onClick={() => {
-                          setActive(row.name);
-                        }}
-                        className="flex md:flex-col flex-row justify-start md:justify-center items-center gap-1">
-                        {row.icon}
-                        <p className={`${active === row.name ? "text-[#1e3a8a]" : "after:"} text-[16px]`}>{row.name}</p>
-                      </a>
-                    </li>
-                  );
-                })}
+                {links.map((row) => (
+                  <li key={row.name}>
+                    <a
+                      href={row.link}
+                      onClick={() => {
+                        setActive(row.name);
+                      }}
+                      className="flex md:flex-col flex-row justify-start md:justify-center items-center gap-1"
+                    >
+                      {row.icon}
+                      <p
+                        className={`${
+                          active === row.name ? 'text-[#1e3a8a]' : 'after:'
+                        } text-[16px]`}
+                      >
+                        {row.name}
+                      </p>
+                    </a>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
@@ -133,18 +144,20 @@ export function Root() {
 
             <div className="text-center text-blue-900 text-5xl pb-5">
               <p>
-                I am skillful <p></p>
+                I am skillful
+                {' '}
+                <p />
                 <TypeAnimation
                   sequence={[
-                    "as a web designer",
+                    'as a web designer',
                     1000,
-                    "in ruby on rails",
+                    'in ruby on rails',
                     1000,
-                    "in postgreSQL",
+                    'in postgreSQL',
                     1000,
-                    "in mySQL",
+                    'in mySQL',
                     1000,
-                    "in nestjs",
+                    'in nestjs',
                     1000,
                   ]}
                   wrapper="span"
@@ -160,15 +173,20 @@ export function Root() {
                 target="_blank"
                 href="https://github.com/yemidada"
                 className="p-2 bg-[#7dd3fc] rounded-full"
-                rel="noreferrer">
+                rel="noreferrer"
+                aria-label="Github"
+              >
                 <BiLogoGithub color="#1e3a8a" size={24} />
+                <span />
               </a>
 
               <a
                 target="_blank"
                 href="https://www.linkedin.com/in/yemidada/"
                 className="p-2 bg-[#7dd3fc] rounded-full"
-                rel="noreferrer">
+                rel="noreferrer"
+                aria-label="LinkedIn"
+              >
                 <BiLogoLinkedinSquare color="#1e3a8a" size={24} />
               </a>
 
@@ -176,7 +194,9 @@ export function Root() {
                 href="https://twitter.com/yemidada"
                 target="_blank"
                 className="p-2 bg-[#7dd3fc] rounded-full"
-                rel="noreferrer">
+                rel="noreferrer"
+                aria-label="Twitter"
+              >
                 <BiLogoTwitter color="#1e3a8a" size={24} />
               </a>
             </div>
@@ -184,12 +204,14 @@ export function Root() {
             <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
               <a
                 href="#contact"
-                className="inline-flex items-center text-white bg-[#1e3a8a] justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full sm:w-auto">
+                className="inline-flex items-center text-white bg-[#1e3a8a] justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full sm:w-auto"
+              >
                 Contact me
               </a>
               <a
                 to="#works"
-                className="inline-flex hover:ring-gray-400 hover:bg-gray-100 items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 mt-3 w-full sm:mt-0 sm:ml-3 sm:w-auto">
+                className="inline-flex hover:ring-gray-400 hover:bg-gray-100 items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 mt-3 w-full sm:mt-0 sm:ml-3 sm:w-auto"
+              >
                 See my work
               </a>
             </div>
@@ -202,3 +224,5 @@ export function Root() {
     </div>
   );
 }
+
+export default Root;
--- src/components/WorkScreen.jsx
+++ src/components/WorkScreen.jsx
@@ -1,49 +1,52 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import WorkWidget from "./WorkWIdget";
-import BudgetImage from "../assets/budget.jpg";
-import HairImage from "../assets/hair.jpg";
-import TodoImage from "../assets/todo.jpg";
-import CalculatorImage from "../assets/calculator.jpg";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import WorkWidget from './WorkWIdget';
+import BudgetImage from '../assets/budget.jpg';
+import HairImage from '../assets/hair.jpg';
+import TodoImage from '../assets/todo.jpg';
+import CalculatorImage from '../assets/calculator.jpg';
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-
-import { Pagination } from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
 
 export default function WorkScreen() {
   const works = [
     {
-      title: "Todo List",
+      id: 1,
+      title: 'Todo List',
       about:
-        "Our intuitive interface simplifies task management. Effortlessly mark tasks as complete using a checkbox (or undo with a simple uncheck). The app automatically saves updates locally, delivering a seamless task management experience crafted with precision in HTML, CSS, and Javascript.",
-      codeLink: "https://github.com/yemidada/todo-list",
-      demoLink: "https://yemidada.github.io/todo-list/dist/",
+        'Our intuitive interface simplifies task management. Effortlessly mark tasks as complete using a checkbox (or undo with a simple uncheck). The app automatically saves updates locally, delivering a seamless task management experience crafted with precision in HTML, CSS, and Javascript.',
+      codeLink: 'https://github.com/yemidada/todo-list',
+      demoLink: 'https://yemidada.github.io/todo-list/dist/',
       image: TodoImage,
     },
     {
-      title: " Hair Style Application",
+      id: 2,
+      title: ' Hair Style Application',
       about:
-        "This is a react capstone project is an adaptive web application that showcases various hairstyles and corresponding hair details sourced from an open API. Developed using React, Redux, HTML, and CSS, this project ensures a responsive user experience.",
-      codeLink: "https://github.com/yemidada/metric-webapp",
-      demoLink: "https://hairstyles.onrender.com",
+        'This is a react capstone project is an adaptive web application that showcases various hairstyles and corresponding hair details sourced from an open API. Developed using React, Redux, HTML, and CSS, this project ensures a responsive user experience.',
+      codeLink: 'https://github.com/yemidada/metric-webapp',
+      demoLink: 'https://hairstyles.onrender.com',
       image: HairImage,
     },
     {
-      title: "My Budget",
+      id: 3,
+      title: 'My Budget',
       about:
-        "This Ruby on Rails capstone project is about building a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what. ",
-      codeLink: "https://github.com/yemidada/MyBudget",
-      demoLink: "https://mybudget-lf89.onrender.com/",
+        'This Ruby on Rails capstone project is about building a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what. ',
+      codeLink: 'https://github.com/yemidada/MyBudget',
+      demoLink: 'https://mybudget-lf89.onrender.com/',
       image: BudgetImage,
     },
     {
-      title: "Math Magicians",
+      id: 4,
+      title: 'Math Magicians',
       about:
-        "Introducing the Math Magician application, meticulously crafted using HTML, CSS, and React. This cutting-edge Single Page App (SPA) empowers users to perform a wide array of mathematical computations effortlessly.",
-      codeLink: "https://github.com/yemidada/math-magicians",
-      demoLink: "https://math-magicians-sngg.onrender.com/",
+        'Introducing the Math Magician application, meticulously crafted using HTML, CSS, and React. This cutting-edge Single Page App (SPA) empowers users to perform a wide array of mathematical computations effortlessly.',
+      codeLink: 'https://github.com/yemidada/math-magicians',
+      demoLink: 'https://math-magicians-sngg.onrender.com/',
       image: CalculatorImage,
     },
   ];
@@ -63,7 +66,7 @@ export default function WorkScreen() {
           pagination={{
             clickable: true,
           }}
-          loop={true}
+          loop
           breakpoints={{
             480: {
               slidesPerView: 1,
@@ -75,21 +78,19 @@ export default function WorkScreen() {
             },
           }}
           modules={[Pagination]}
-          className="mySwiper">
-          {works.map((work, index) => {
-            return (
-              <SwiperSlide>
-                <WorkWidget
-                  key={index}
-                  title={work.title}
-                  codeLink={work.codeLink}
-                  demoLink={work.demoLink}
-                  about={work.about}
-                  image={work.image}
-                />
-              </SwiperSlide>
-            );
-          })}
+          className="mySwiper"
+        >
+          {works.map((work) => (
+            <SwiperSlide key={work.id}>
+              <WorkWidget
+                title={work.title}
+                codeLink={work.codeLink}
+                demoLink={work.demoLink}
+                about={work.about}
+                image={work.image}
+              />
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </>
